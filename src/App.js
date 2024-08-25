8d12df951cfaee64ffce626b1cb398086ebbb6d9
Van4xx <van4xx@ya.ru>
2024-08-25 07:40:01 +0300

    add components in App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,28 @@
+import React, { useState } from 'react';
 import './App.scss';
+import Balance from './components/Balance';
+import TransactionList from './components/TransactionList';
+import TransactionForm from './components/TransactionForm';
 
+function App() {
+  const [transactions, setTransactions] = useState([]);
+  
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  const calculateBalance = () => {
+    return transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+  };
+
+  return (
+    <div className="App container">
+      <h1>Finance Tracker</h1>
+      <Balance balance={calculateBalance()} />
+      <TransactionList transactions={transactions} />
+      <TransactionForm addTransaction={addTransaction} />
+    </div>
+  );
+}
+
+export default App;
