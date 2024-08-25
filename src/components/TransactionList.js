51d321f0de0f1e29aaea2009516d917e425f8413
Van4xx <van4xx@ya.ru>
2024-08-25 07:53:17 +0300

    change TransactionList add delete transaction and delete node-sass

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,18 +1,20 @@
-import React from 'react'
+// src/components/TransactionList.js
+import React from 'react';
 
-const TransactionList = ({transactions}) => {
-    return (
-        <div className='transaction-list'>
-            <h3>Transaction</h3>
-            <ul>
-                {transactions.map((transaction, index) => (
-                <li key={index} className={transaction.amount < 0 ? 'expense' : 'income'}>
-                    {transaction.description} <span>${transaction.amount.toFixed(2)}</span>
-                </li>
-                ))}
-            </ul>
-        </div>
-    );
+const TransactionList = ({ transactions, deleteTransaction }) => {
+  return (
+    <div className="transaction-list">
+      <h3>Transactions</h3>
+      <ul>
+        {transactions.map((transaction, index) => (
+          <li key={index} className={transaction.amount < 0 ? 'expense' : 'income'}>
+            {transaction.description} <span>${transaction.amount.toFixed(2)}</span>
+            <button className="btn btn-danger btn-sm" onClick={() => deleteTransaction(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default TransactionList;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.scss';
+import App from './App.js';
 
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
